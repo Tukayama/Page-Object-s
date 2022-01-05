f5d599945c3f8c3ab443f0dad0c1a04d0ad25811
Tukayama <ya.tumanov2009@yandex.ru>
2022-01-05 17:34:18 +0300

    init commit

---
+++ src/test/java/ru/netology/web/page/DashboardPage.java
@@ -0,0 +1,28 @@
+package ru.netology.web.page;
+
+import com.codeborne.selenide.ElementsCollection;
+import lombok.val;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+public class DashboardPage {
+    private ElementsCollection cards = $$(".list__item");
+    private final String balanceStart = "баланс: ";
+    private final String balanceFinish = " р.";
+
+    public void Dashboard() {
+    }
+
+    public int getFirstCardBalance() {
+        val text = cards.first().text();
+        return extractBalance(text);
+    }
+
+    private int extractBalance(String text) {
+        val start = text.indexOf(balanceStart);
+        val finish = text.indexOf(balanceFinish);
+        val value = text.substring(start + balanceStart.length(), finish);
+        return Integer.parseInt(value);
+    }
+}
